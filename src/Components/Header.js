8cd3dc6b60b120e142471791b505f834316329a0
Ejaz Shaikh <ejaz8837@gmail.com>
2023-04-17 02:03:06 +0530

    update slider

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,14 +5,14 @@ function Header() {
     return (
         <div>
             <section>
-                <nav className="navbar navbar-expand-lg bg-body-white py-3">
+                <nav className="navbar navbar-expand-lg bg-body-white py-3  bg-white">
                     <div className="container-fluid">
                         <img src={logo} alt="website logo" />
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav ms-auto mb-2">
+                        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                            <ul className="navbar-nav ms-auto mb-2 ">
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         English
